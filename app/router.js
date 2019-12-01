1a604f02f1d22a4a3c6bc4a4c43655c3cc1c0d93
Grace Lee <igracelee1@gmail.com>
2019-11-30 23:24:56 -0500

    first commit

--- app/router.js
+++ app/router.js
@@ -7,4 +7,19 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
+  // this.route('about, {path:'about-me'});
+  this.route('about');
+  this.route('services');
+  this.route('contact');
+
+  /*
+    the 'posts' template in the 'templates' directory
+    has an index.hbs file, which is the 'posts' template
+    itself
+  */
+  this.route('posts', {path:'/posts'}, function() {
+    this.route('new'); // localhost:4200/posts/new
+    // get a single post -- format the route
+    this.route('post', {path: ':post_id'}) // localhost:4200/posts/2
+  })
 });
