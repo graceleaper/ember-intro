c46b52e2dda75b209f8fc8659e7ac61eb0bba49a
Grace Lee <igracelee1@gmail.com>
2020-02-13 22:00:40 -0500

    Create controller and template for single post

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -3,6 +3,7 @@ import Controller from '@ember/controller';
 export default Controller.extend({
     title: 'My first blog post',
     body: 'This is my first blog post, and I am exited to share it with you all!',
+    // prevent properties from being changed with Object.freeze()
     authors: Object.freeze(['William', 'Robert', 'Michelle']),
     comments: Object.freeze([
         {
